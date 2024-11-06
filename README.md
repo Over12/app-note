7906fce0e9d699d95041599052dc25dea1a88f7d
OverCode <avalosfigueroapedro@gmail.com>
2024-11-06 16:57:28 -0500

    Renombrar la aplicación de JOTTER a MEMOIZER y actualizar el título y la navbar en consecuencia

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# JOTTER
+# MEMOIZER
 
 Esta es una página simple que creé para practicar mis habilidades en React, en la misma se guardará notas en localStorage, además de poder eliminarlas.
--- src/assets/components/ui/navbar.jsx
+++ src/assets/components/ui/navbar.jsx
@@ -16,7 +16,7 @@ export default function NavBar () {
     <nav className="px-10 py-5 border-b-2 border-b-text/20 flex justify-between items-center">
       <div className="flex items-center gap-1" href="#">
         <img className="size-12" src="/icon.svg" alt="Icono de la página Jotter" />
-        <span className="text-3xl font-bold">JOTTER</span>
+        <span className="text-3xl font-bold">MEMOIZER</span>
       </div>
       <button className="bg-primary/50 px-4 py-2 rounded-md border-2 border-primary hover:bg-transparent transition-colors" onClick={() => handleNewNote()}>
         Nueva nota
